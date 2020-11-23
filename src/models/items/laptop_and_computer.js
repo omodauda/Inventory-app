1281dde565748bf8aed73ae860d81d7556e00f16
omodauda <omodauda.dl@gmail.com>
2020-11-23 11:44:30 +0100

    create laptops and computers model

---
+++ src/models/items/laptop_and_computer.js
@@ -0,0 +1,84 @@
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+
+const laptopAndComputerSchema = new schema({
+    category: {
+        type: String,
+        required: true
+    },
+    owner: {
+        type: schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    itemImages: {
+        images: [{
+            type: String
+        }],
+        cloudinary_ids: [{
+            type: String
+        }]
+    },
+    location: {
+        type: String
+    },
+    type: {
+        type: String,
+        required: true,
+        enum: ["Laptop", "Desktop Computer"]
+    },
+    brand: {
+        type: String,
+        required: true,
+    },
+    model: {
+        type: String,
+        required: true
+    },
+    condition:{
+        type: String,
+        enum: ["New", "Used", "Refurbished"]
+    },
+    price: {
+        type: String
+    },
+    processor: {
+        type: String
+    },
+    numberOfCores: {
+        type: String
+    },
+    ram: {
+        type: String
+    },
+    storageCapacity: {
+        type: String
+    },
+    storageType: {
+        type: String
+    },
+    displaySize: {
+        type: String
+    },
+    graphicCard: {
+        type: String,
+    },
+    graphicCardMemory: {
+        type: String
+    },
+    os: {
+        type: String
+    },
+    description: {
+        type: String
+    },
+    status: {
+        type: String,
+        default: "Reviewing",
+        enum: ["Active", "Reviewing", "Closed", "Declined"]
+    },
+},{timestamps: true});
+
+const LaptopAndComputer = mongoose.model('LaptopAndComputer', laptopAndComputerSchema);
+
+module.exports = LaptopAndComputer;
